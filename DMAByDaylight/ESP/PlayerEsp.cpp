70f63dccda367c7324a406d357201c9417539ce5
IntelSDM <moshkathechad@gmail.com>
2024-05-01 21:18:24 +0100

    ESP Config Implementation

--- DMAByDaylight/ESP/PlayerEsp.cpp
+++ DMAByDaylight/ESP/PlayerEsp.cpp
@@ -5,6 +5,7 @@
 #include "ActorEntity.h"
 #include "Engine.h"
 #include "PlayerEsp.h"
+#include "ConfigUtilities.h"
 
 std::shared_ptr<CheatFunction> UpdatePlayers = std::make_shared<CheatFunction>(10, [] {
 	if (!EngineInstance)
@@ -16,17 +17,26 @@ void DrawPlayerEsp()
 {
 	if (!EngineInstance)
 		return;
-		for (auto entity : EngineInstance->GetActors())
-		{
-			if (entity->GetPosition() == Vector3(0, 0, 0))
-				continue;
-			Vector2 screenpos = Camera::WorldToScreen(EngineInstance->GetCameraCache().POV, entity->GetPosition());
-			Vector3 campos = Vector3(EngineInstance->GetCameraCache().POV.Location.X, EngineInstance->GetCameraCache().POV.Location.Y, EngineInstance->GetCameraCache().POV.Location.Z);
-			float distance = (Vector3::Distance(campos, entity->GetPosition()) / 39.62f) - 6;
-			if (distance < 0)
-				continue;
-			std::wstring wdistance = L"[" + std::to_wstring((int)distance) + L"m]";
-			DrawText(screenpos.x, screenpos.y, entity->GetName() + wdistance, "Verdana", 12, Colour(255, 0, 0, 255), CentreCentre);
-		}
+	for (auto entity : EngineInstance->GetActors())
+	{
+		if (entity->GetPosition() == Vector3(0, 0, 0))
+			continue;
+		PlayerConfig config = Configs.Survivor;
+		if (entity->GetPlayerRole() == EPlayerRole::EPlayerRole__VE_Slasher)
+			config = Configs.Killer;
+
+		Vector2 screenpos = Camera::WorldToScreen(EngineInstance->GetCameraCache().POV, entity->GetPosition());
+		if (screenpos == Vector2::Zero())
+			continue;
+		Vector3 campos = Vector3(EngineInstance->GetCameraCache().POV.Location.X, EngineInstance->GetCameraCache().POV.Location.Y, EngineInstance->GetCameraCache().POV.Location.Z);
+		float distance = (Vector3::Distance(campos, entity->GetPosition()) / 39.62f) - 6;
+		if (distance < 0)
+			continue;
+		std::wstring wdistance = config.Distance ? L"[" + std::to_wstring((int)distance) + L"m]" : L"";
+		std::wstring name = config.Name ? entity->GetName() : L"";
+		if (distance > config.MaxDistance)
+			continue;
+		DrawText(screenpos.x, screenpos.y, name + wdistance, "Verdana", config.FontSize, config.TextColour, CentreCentre);
+	}
 	
 }
